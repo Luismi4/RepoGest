09ef76ff8f8ca29acd179bebb149fc5506aaddd2
kike <ebellidoperejil2@gmail.com>
2023-09-21 19:23:04 +0200

    Eliminar termiando

--- Gestor_Tareas/src/Gestion/Eliminar.java
+++ Gestor_Tareas/src/Gestion/Eliminar.java
@@ -1,5 +1,32 @@
 package Gestion;
 
-public class Eliminar {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Eliminar {
+	static Scanner leer = new Scanner(System.in);
+	public static void EliminarTarea (ArrayList<Tarea> ta) {
+		String titulo;
+        System.out.println("Introduce el título de la tarea que quieres eliminar:");
+        Listar.List(ta); // Suponiendo que Listar.List() muestra la lista de tareas
+
+        titulo = leer.next();
+
+        boolean tareaEncontrada = false;
+
+        for (int i = 0; i < ta.size(); i++) {
+            if (ta.get(i).getTit().equals(titulo)) {
+                tareaEncontrada = true;
+                ta.remove(i);
+                System.out.println("Tarea eliminada exitosamente.");
+                break; // Salimos del bucle una vez que encontramos la tarea
+            }
+        }
+
+        if (!tareaEncontrada) {
+            System.out.println("Título de tarea no encontrado en la lista.");
+        }
+    }
 }
+
+
