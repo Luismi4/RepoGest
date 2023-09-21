f51aff3c2222bd4897174242cd3a7ffce5a6c7fc
kike <ebellidoperejil2@gmail.com>
2023-09-21 18:42:58 +0200

    pasar menu de eje a Menu

--- Gestor_Tareas/src/Gestion/Eje.java
+++ Gestor_Tareas/src/Gestion/Eje.java
@@ -1,45 +1,10 @@
 package Gestion;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Eje {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int opcion;
-
-		do {
-			System.out.println("\n*** Menú de Tareas ***");
-			System.out.println("1. Crear Tarea");
-			System.out.println("2. Listar Tareas");
-			System.out.println("3. Modificar Tarea");
-			System.out.println("4. Eliminar Tarea");
-			System.out.println("5. Salir");
-			System.out.print("Seleccione una opción: ");
-
-			opcion = scanner.nextInt();
-
-			switch (opcion) {
-			case 1:
-				
-				break;
-			case 2:
-				
-				break;
-			case 3:
-				
-				break;
-			case 4:
-				
-				break;
-			case 5:
-				System.out.println("¡Hasta luego!");
-				break;
-			default:
-				System.out.println("Opción no válida. Intente nuevamente.");
-			}
-		} while (opcion != 5);
-		scanner.close();
 	}
 }
 
--- Gestor_Tareas/src/Gestion/Menu.java
+++ Gestor_Tareas/src/Gestion/Menu.java
@@ -1,8 +1,49 @@
 package Gestion;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
+	
+	public static void MenuTarea() {
+		Scanner scanner = new Scanner(System.in);
+		int opcion;
+		ArrayList<Tarea> ta = new ArrayList<>();
+		do {
+			System.out.println("\n*** Menú de Tareas ***");
+			System.out.println("1. Crear Tarea");
+			System.out.println("2. Listar Tareas");
+			System.out.println("3. Modificar Tarea");
+			System.out.println("4. Eliminar Tarea");
+			System.out.println("5. Salir");
+			System.out.print("Seleccione una opción: ");
 
-}
+			opcion = scanner.nextInt();
+
+			switch (opcion) {
+			case 1:
+				Anadir.AniaUs(ta);
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			case 5:
+				System.out.println("¡Hasta luego!");
+				break;
+			default:
+				System.out.println("Opción no válida. Intente nuevamente.");
+			}
+		} while (opcion != 5);
+		scanner.close();
+	}
+	}
+	
+	
+
 
