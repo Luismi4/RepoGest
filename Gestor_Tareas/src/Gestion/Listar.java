305f82ace129a94f8bde57e7231e2e3b9545b3b6
Luismi <luismi.loalv@gmail.com>
2023-09-21 19:24:55 +0200

    Listar y toString

--- Gestor_Tareas/src/Gestion/Listar.java
+++ Gestor_Tareas/src/Gestion/Listar.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 public class Listar {
 
-	public static void List(ArrayList<Tarea> ta) {
-		
+	public static void List(ArrayList<Tarea> ta) {	
 		for (Tarea ele : ta) {
             System.out.println(ele.toString());
 		}
